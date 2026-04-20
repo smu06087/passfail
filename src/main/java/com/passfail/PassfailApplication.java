69411cac098f2373c59c1f99b04046d95fc4425e
smu06087 <smu06089@gmail.com>
2026-04-20 14:16:50 +0900

    first commit

---
+++ src/main/java/com/passfail/PassfailApplication.java
@@ -0,0 +1,13 @@
+package com.passfail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassfailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassfailApplication.class, args);
+	}
+
+}
